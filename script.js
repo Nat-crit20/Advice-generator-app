1acedb55f3fa80fe352cd99b82add45619ba163c
Nathan Greuel <greuelnathan74@gmail.com>
2022-09-06 11:03:41 -0500

    Finished fetch javascript

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const adviceNum = document.querySelector(".advice-num");
+const advice = document.querySelector(".advice");
+const button = document.querySelector("button");
+
+const getAdvice = async function () {
+  const response = await fetch("https://api.adviceslip.com/advice").then(
+    (data) => data.json()
+  );
+  console.log(response.slip);
+  return response.slip;
+};
+
+button.addEventListener("click", function () {
+  let advice = getAdvice();
+});
